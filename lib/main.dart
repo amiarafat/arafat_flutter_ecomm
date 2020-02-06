405b8e0f298bd16917a3efc22215a54f3a8d2735
Hossain Md. Arafat <arafat@Hossains-MacBook-Pro.local>
2020-02-06 13:38:24 +0600

    2nd video and column and row widget basic

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     new MaterialApp(
-      home: new Container(
-        color: Colors.white,
-        child: Text('Hello World Two')
+      home: new Center(
+        child: Row(
+          children: <Widget>[
+            Text('Hello one ',style:  TextStyle(fontSize: 20),),
+            Text('Hello two ',style:  TextStyle(fontSize: 20),),
+            Text('Hello three',style:  TextStyle(fontSize: 20),),
+            //Text('Hello three'),
+          ],
+        )
       )
     )
   );
