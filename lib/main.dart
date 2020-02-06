45af13320fe8bd424aa99cfa08716451c479985c
Hossain Md. Arafat <arafat@Hossains-MacBook-Pro.local>
2020-02-06 14:50:57 +0600

    3rd video and list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main(){
+
   runApp(
-    new MaterialApp(
-      home: new Center(
-        child: Row(
+    MaterialApp(
+      home: Center(
+        child: ListView(
           children: <Widget>[
-            Text('Hello one ',style:  TextStyle(fontSize: 20),),
-            Text('Hello two ',style:  TextStyle(fontSize: 20),),
-            Text('Hello three',style:  TextStyle(fontSize: 20),),
-            //Text('Hello three'),
+            Text('one'),
+            Text('one'),
           ],
-        )
-      )
+        ),
+      ),
     )
   );
 }
